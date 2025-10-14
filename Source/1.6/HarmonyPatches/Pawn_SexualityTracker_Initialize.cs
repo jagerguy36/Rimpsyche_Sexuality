bdf4d04973736001c265698d1654af787bfc75fe
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-10-14 17:44:35 +0900

    Feat: General work

--- Source/1.6/HarmonyPatches/Pawn_SexualityTracker_Initialize.cs
+++ Source/1.6/HarmonyPatches/Pawn_SexualityTracker_Initialize.cs
@@ -12,63 +12,46 @@ namespace Maux36.RimPsyche.Sexuality
         static bool Prefix(Pawn_SexualityTracker __instance, Pawn pawn, bool rewrite)
         {
             //Already initialized before
-            if (__instance.orientationCategory != SexualOrientation.None && __instance.orientationCategory != SexualOrientation.Developing)
-            {
-                return false;
-            }
+            if (__instance.orientationCategory != SexualOrientation.None && __instance.orientationCategory != SexualOrientation.Developing) return false;
             var traits = pawn.story?.traits;
             var gender = pawn.gender;
-            if (traits != null && gender != Gender.None)
+            //Not Applicable
+            if (traits == null || gender == Gender.None) return false;
+            if (pawn.ageTracker.AgeBiologicalYears < Rimpsyche_Utility.GetMinAdultAge(pawn))
             {
-                if (pawn.ageTracker.AgeBiologicalYears < Rimpsyche_Utility.GetMinAdultAge(pawn))
-                {
-                    __instance.orientationCategory = SexualOrientation.Developing;
-                    return false;
-                }
+                __instance.orientationCategory = SexualOrientation.Developing;
+                return false;
+            }
 
-                //Decide sexuality
-                if (traits.HasTrait(TraitDefOf.Gay))
+            //Decide sexuality
+            if (traits.HasTrait(TraitDefOf.Gay)) __instance.orientationCategory = SexualOrientation.Homosexual;
+            else if (traits.HasTrait(TraitDefOf.Bisexual)) __instance.orientationCategory = SexualOrientation.Bisexual;
+            else if (traits.HasTrait(TraitDefOf.Asexual)) __instance.orientationCategory = SexualOrientation.Asexual;
+            else __instance.orientationCategory = SexualOrientation.Heterosexual;
+
+            __instance.kinsey = SexualityHelper.GenerateKinseyFor(__instance.orientationCategory);
+            if (__instance.kinsey < 0) //Un initialized.
+            {
+                Log.Error("Kinsey is uninitialized");
+                return false;
+            }
+            else
+            {
+                //TODO: Rework sexdrive
+                __instance.sexDrive = SexualityHelper.GenerateSexDriveFor(__instance.orientationCategory);
+                float attraction = SexualityHelper.GenerateAttractionFor(__instance.orientationCategory);
+                float forSame = __instance.kinsey;
+                float forDiff = 1f - __instance.kinsey;
+                float multiplier = attraction / Mathf.Max(forSame, forDiff);
+                if (gender == Gender.Male)
                 {
-                    __instance.orientationCategory = SexualOrientation.Homosexual;
-                }
-                else if (traits.HasTrait(TraitDefOf.Bisexual))
-                {
-                    __instance.orientationCategory = SexualOrientation.Bisexual;
-                }
-                else if (traits.HasTrait(TraitDefOf.Asexual))
-                {
-                    __instance.orientationCategory = SexualOrientation.Asexual;
+                    __instance.mAattraction = multiplier * forSame;
+                    __instance.fAattraction = multiplier * forDiff;
                 }
                 else
                 {
-                    __instance.orientationCategory = SexualOrientation.Heterosexual;
-                }
-
-
-                __instance.kinsey = SexualityHelper.GenerateKinseyFor(__instance.orientationCategory);
-                if (__instance.kinsey < 0) //Un initialized.
-                {
-                    Log.Error("Kinsey is uninitialized");
-                    return false;
-                }
-                else
-                {
-                    //TODO: Rework sexdrive
-                    __instance.sexDrive = SexualityHelper.GenerateSexDriveFor(__instance.orientationCategory);
-                    float attraction = SexualityHelper.GenerateAttractionFor(__instance.orientationCategory);
-                    float forSame = __instance.kinsey;
-                    float forDiff = 1f - __instance.kinsey;
-                    float multiplier = attraction / Mathf.Max(forSame, forDiff);
-                    if (gender == Gender.Male)
-                    {
-                        __instance.mAattraction = multiplier * forSame;
-                        __instance.fAattraction = multiplier * forDiff;
-                    }
-                    else
-                    {
-                        __instance.mAattraction = multiplier * forDiff;
-                        __instance.fAattraction = multiplier * forSame;
-                    }
+                    __instance.mAattraction = multiplier * forDiff;
+                    __instance.fAattraction = multiplier * forSame;
                 }
             }
             return false;
--- Source/1.6/SexualityHelper.cs
+++ Source/1.6/SexualityHelper.cs
@@ -4,78 +4,90 @@ namespace Maux36.RimPsyche.Sexuality
 {
     public class SexualityHelper
     {
-    //TODO: Look up distribution researches
-    public List<float> Distribution = new List<float> { 0.6f, 0.15f, 0.1f, 0.05f, 0.05f, 0.01f, 0.04f };
-    public static readonly SimpleCurve SexualityCurve = CreateSexualityCurve();
-    private static SimpleCurve CreateSexualityCurve()
-    {
-        List<CurvePoint> curvePoints = new List<CurvePoint>();
-        List<float> yValues = new List<float> { 0f, 0.2f, 0.4f, 0.6f, 0.8f, 1f };
-        float cumulativeX = 0f;
-        int yIndex = 0;
-
-        for (int i = 0; i < Distribution.Count-1; i++)
+        //TODO: Look up distribution researches
+        public static List<float> Distribution = new List<float> { 0.6f, 0.15f, 0.1f, 0.05f, 0.05f, 0.01f, 0.04f };
+        public const List<float> steps = new List<float> { 0f, 0.2f, 0.4f, 0.6f, 0.8f, 1f };
+        private static readonly float StraightSum = Distribution[0]+Distribution[1];
+        private static readonly float BiSum = Distribution[2]+Distribution[3]+Distribution[4];
+        private static readonly float GaySum = Distribution[5]+Distribution[6];
+        public static readonly SimpleCurve SexualityCurve = CreateSexualityCurve();
+        private static SimpleCurve CreateSexualityCurve()
         {
-            cumulativeX += Distribution[i];
-            curvePoints.Add(new CurvePoint(cumulativeX, yValues[i]));
+            List<CurvePoint> curvePoints = new List<CurvePoint>();
+            float cumulativeX = 0f;
+            for (int i = 0; i < Distribution.Count-1; i++)
+            {
+                cumulativeX += Distribution[i];
+                curvePoints.Add(new CurvePoint(cumulativeX, steps[i]));
+            }
+            return new SimpleCurve(curvePoints.ToArray());
         }
-        return new SimpleCurve(curvePoints.ToArray());
-    }
-
         public static readonly SimpleCurve StraightCurve = new SimpleCurve
         {
-            new CurvePoint(0.8f, 0f),
-            new CurvePoint(1f, 0.2f)
+            new CurvePoint(Distribution[0]/StraightSum, step[0]),
+            new CurvePoint(1f, step[1])
         };
         public static readonly SimpleCurve BiCurve = new SimpleCurve
         {
-            new CurvePoint(0f, 0.2f),
-            new CurvePoint(0.5, 0.4f)
-            new CurvePoint(0.75f, 0.6f)
-            new CurvePoint(1f, 0.8f)
+            new CurvePoint(0f, steps[1]),
+            new CurvePoint(Distribution[2]/BiSum, steps[2])
+            new CurvePoint((Distribution[2]+Distribution[3])/BiSum, steps[3])
+            new CurvePoint(1f, steps[4])
         };
         public static readonly SimpleCurve GayCurve = new SimpleCurve
         {
-            new CurvePoint(0f, 0.8f),
-            new CurvePoint(0.2f, 1f)
+            new CurvePoint(0f, steps[4]),
+            new CurvePoint(Distribution[5]/GaySum, steps[5])
         };
 
         public static float GenerateKinseyFor(SexualOrientation orientation)
         {
             float kinsey = -1f;
-            int safetyMaxtries = 10;
-            int tryCounter = 0;
+            float flatRatio = Rand.Range(0f, 1f);
             switch (orientation)
             {
                 case SexualOrientation.Heterosexual:
-                    kinsey = Rand.Range(0f, 0.15f);
+                    kinsey = StraightCurve.Evaluate(flatRatio);
                     break;
                 case SexualOrientation.Bisexual:
-                    kinsey = Rand.Range(0.15f, 0.85f);
-                    while ((kinsey == 0.15f || kinsey == 0.85f) && tryCounter < safetyMaxtries)
-                    {
-                        kinsey = Rand.Range(0.15f, 0.85f);
-                        tryCounter++;
-                    }
-                    break;
+                    kinsey = BiCurve.Evaluate(flatRatio);
                 case SexualOrientation.Homosexual:
-                    kinsey = Rand.Range(0.85f, 1f);
+                    kinsey = GayCurveCurve.Evaluate(flatRatio);
                     break;
                 case SexualOrientation.Asexual:
-                    kinsey = Rand.Range(0, 1f);
+                    kinsey = SexualityCurve.Evaluate(flatRatio);
                     break;
             }
             return kinsey;
         }
+        public static float ReEvaluateKinsey(float sameAttraction, float diffAttraction)
+        {
+            return sameAttraction / (sameAttraction + diffAttraction);
+        }
+
         public static float GenerateSexDriveFor(SexualOrientation orientation)
         {
             if (orientation == SexualOrientation.Asexual) return Rand.Range(0, 0.05f);
-            else return Rand.Range(0.05f, 1f);
+            else return GetNormalDistribution(0.05f, 1f);
         }
         public static float GenerateAttractionFor(SexualOrientation orientation)
         {
             if (orientation == SexualOrientation.Asexual) return Rand.Range(0, 0.05f);
-            else return Rand.Range(0.05f, 1f);
+            else return GetNormalDistribution(0.05f, 1f);
+        }
+        public float GetNormalDistribution(float lowBracket = 0f, float highBracket = 1f, int maxAttempts = 4)
+        {
+            float result;
+            int attempts = 0;
+            do
+            {
+                result = Rand.Gaussian(0.5f, 0.2f);
+                attempts++;
+            }
+            while ((result < lowBracket || result > highBracket) && attempts < maxAttempts); 
+
+            if (result < lowBracket || result > highBracket)result = Mathf.Clamp(result, lowBracket, highBracket);
+            return result;
         }
     }
 }

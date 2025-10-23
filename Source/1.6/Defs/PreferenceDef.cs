995e47f73cb5cebd725420f7df6a30820656aecb
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-10-23 10:29:19 +0900

    Feat: PrefDef setup

--- Source/1.6/Defs/PreferenceDef.cs
+++ Source/1.6/Defs/PreferenceDef.cs
@@ -2,7 +2,46 @@
 
 namespace Maux36.RimPsyche.Sexuality
 {
+    public enum RimpsychePrefCategory
+    {
+        Physical,
+        Social,
+        Romantic,
+        Misc
+    }
+    public enum RimpsycheCacheType
+    {
+        Once,
+        Never,
+        Psyche
+    }
     public class PreferenceDef: Def
     {
+        public RimpsychePrefCategory category
+        public RimpsycheCacheType cacheType
+        public Type workerClass = typeof(PreferenceWorker);
+
+        [Unsaved(false)]
+        private PreferenceWorker workerInt;
+
+        public PreferenceWorker Worker
+        {
+            get
+            {
+                if (workerInt == null)
+                {
+                    workerInt = (PreferenceWorker)Activator.CreateInstance(workerClass);
+                    workerInt.def = this;
+                }
+                return workerInt;
+            }
+        }
+    }
+
+    public abstract class PreferenceWorker
+    {
+        public abstract void SetUp(Pawn pawn);
+
+        public abstract float Evaluate(Pawn obesrver, Pawn target);
     }
 }

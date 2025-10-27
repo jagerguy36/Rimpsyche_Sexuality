576eae7b8d0ce899f5e82c2e112b5c86f53fc855
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-10-28 00:03:28 +0900

    Feat: Preference work

---
+++ Source/1.6/Preferences/PreferenceDef.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality
+{
+    public class PreferenceDef: Def
+    {
+        public RimpsychePrefCategory category;
+        public RimpsycheCacheType cacheType;
+        public Type workerClass = typeof(PreferenceWorker);
+
+        [Unsaved(false)]
+        private PreferenceWorker workerInt;
+
+        public PreferenceWorker Worker
+        {
+            get
+            {
+                if (workerInt == null)
+                {
+                    workerInt = (PreferenceWorker)Activator.CreateInstance(workerClass);
+                    workerInt.def = this;
+                }
+                return workerInt;
+            }
+        }
+    }
+    public enum RimpsychePrefCategory
+    {
+        Physical,
+        Social,
+        Romantic,
+        Misc
+    }
+    public enum RimpsycheCacheType
+    {
+        Once,
+        Never,
+        Psyche
+    }
+    public abstract class PreferenceWorker
+    {
+        public float EditorHeight;
+        public PreferenceDef def;
+
+        public abstract void SetUp(Pawn pawn);
+        
+        public abstract string Report(Pawn pawn);
+
+        public abstract float Evaluate(Pawn obesrver, Pawn target);
+
+        public abstract void DrawEditor(Rect rect, Pawn pawn);
+    }
+}
---
+++ Source/1.6/Preferences/PsychePreferenceWorker.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality
+{
+    public class PsychePerferenceWorker : PreferenceWorker
+    {
+        private static readonly List<PersonalityDef> SourceDefs = DefDatabase<PersonalityDef>.AllDefsListForReading;
+        private static readonly int nodeCount = SourceDefs.Count;
+        private static readonly int[] IndexPool = Enumerable.Range(0, nodeCount).ToArray();
+        public PsychePerferenceWorker()
+        {
+            EditorHeight = 100f;
+        }
+
+        public static List<PersonalityDef> RandomFiveNodes()
+        {
+            const int selectCount = 5;
+            for (int i = 0; i < selectCount; i++)
+            {
+                int randIndex = Rand.Range(i, nodeCount);
+                (IndexPool[i], IndexPool[randIndex]) = (IndexPool[randIndex], IndexPool[i]);
+            }
+            List<PersonalityDef> result = new List<PersonalityDef>(selectCount);
+            for (int i = 0; i < selectCount; i++)
+            {
+                result.Add(SourceDefs[IndexPool[i]]);
+            }
+            return result;
+        }
+        public override void SetUp(Pawn pawn)
+        {
+            var compPsyche = pawn.compPsyche();
+            if (compPsyche?.Enabled != true) return;
+            var relevantNodes = RandomFiveNodes();
+            for (int i = 0; i < relevantNodes.Count; i++)
+            {
+                float importance = Rand.Range(0f, 1f);
+
+            }
+
+        }
+
+        public override string Report(Pawn pawn)
+        {
+            return "";
+        }
+
+        public override float Evaluate(Pawn obesrver, Pawn target)
+        {
+            return 0f;
+        }
+
+        public override void DrawEditor(Rect rect, Pawn pawn)
+        {
+            return;
+        }
+    }
+}

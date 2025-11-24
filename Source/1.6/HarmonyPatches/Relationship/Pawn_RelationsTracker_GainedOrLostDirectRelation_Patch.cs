c653409e797a71c35b082c132645fe6e8172c8d1
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-11-25 00:29:02 +0900

    Feat: Relation cache

---
+++ Source/1.6/HarmonyPatches/Relationship/Pawn_RelationsTracker_GainedOrLostDirectRelation_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality
+{
+    [HarmonyPatch(typeof(Pawn_RelationsTracker), "GainedOrLostDirectRelation")]
+    public static class Pawn_RelationsTracker_GainedOrLostDirectRelation_Patch
+    {
+        public static void Postfix(Pawn ___pawn)
+        {
+            var pawnPsyche = ___pawn.compPsyche();
+            if (pawnPsyche?.Enabled != true) return;
+            pawnPsyche.Sexuality.DirtyLoversCache();
+        }
+    }
+}

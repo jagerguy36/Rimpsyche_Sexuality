f087c48422caf3de5dee27b7eb86c5beb6549be5
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-11-30 02:43:14 +0900

    Feat: Memory management

---
+++ Source/1.6/HarmonyPatches/Pawn_Discard_Patch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality
+{
+    [HarmonyPatch(typeof(Pawn), nameof(Pawn.Discard))]
+    public static class Pawn_Discard_Patch
+    {
+        public static void Postfix(Pawn __instance)
+        {
+            VersionManager.DiscardedPawnThingIDnumber.Add(__instance.thingIDNumber);
+        }
+    }
+}

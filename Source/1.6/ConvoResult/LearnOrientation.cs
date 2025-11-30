bf8c99b4dd0aa818a44011b1a122d90f901becea
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-11-30 13:26:22 +0900

    Feat: Learn orientation through some conversation

---
+++ Source/1.6/ConvoResult/LearnOrientation.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace Maux36.RimPsyche.Sexuality
+{
+    public class LearnOrientation: ConvoResultBase
+    {
+        public float learningChance = 1f;
+        public override void ApplyEffect(Pawn initiator, Pawn recipient, float alignment, float initOpinionOffset, float reciOpinionOffset)
+        {
+            if (Rand.Value < learningChance)
+            {
+                initiator.compPsyche().Sexuality.LearnOrientationOf(recipient);
+            }
+        }
+    }
+}

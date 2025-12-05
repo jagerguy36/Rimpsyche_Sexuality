b6802750d558c38ad33233e1f3eb77b445770faa
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-05 21:23:49 +0900

    Feat: Intimacy sexdrive further integration

---
+++ Source/Mods/Rimpsyche_Intimacy_Compat/SexDrive/InteractionWorker_Seduce_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using LoveyDoveySexWithEuterpe;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_Intimacy_Compat
+{
+    [HarmonyPatch(typeof(InteractionWorker_Seduce), nameof(InteractionWorker_Seduce.RandomSelectionWeight))]
+    public static class InteractionWorker_Seduce_Patch
+    {
+        public static void Postfix(ref float __result, Pawn initiator, Pawn recipient)
+        {
+            if (__result == 0) return;
+            var compPsyche = initiator.compPsyche();
+            if (compPsyche.Enabled != true) return;
+            __result *= compPsyche.Sexuality.GetAdjustedSexdrive();
+        }
+    }
+}
---
+++ Source/Mods/Rimpsyche_Intimacy_Compat/SexDrive/RimpsycheIntimacySexdriveStatPart.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_Intimacy_Compat
+{
+    public class RimpsycheIntimacySexdriveStatPart : StatPart
+    {
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (req.HasThing && req.Thing is Pawn pawn)
+            {
+                var compPsyche = pawn.compPsyche();
+                if (compPsyche?.Enabled == true)
+                {
+                    val *= 1.5f - compPsyche.Sexuality.SexDrive;
+                }
+            }
+        }
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (req.HasThing && req.Thing is Pawn pawn)
+            {
+                var compPsyche = pawn.compPsyche();
+                if (compPsyche?.Enabled == true)
+                {
+                    return "RP_Stat_Psyche".Translate() + "\n    " + "RP_Stat_Sexdrive".Translate() + ": x" + (1.5f - compPsyche.Sexuality.SexDrive).ToStringPercent() + "\n";
+                }
+            }
+            return null;
+        }
+
+    }
+}

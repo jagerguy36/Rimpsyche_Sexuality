de0fa7f01af7bd2eb20f6e115222a4943e807a4c
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-06 21:07:31 +0900

    Feat: setting and image

--- Source/1.6/RimpsycheSexuality.cs
+++ Source/1.6/RimpsycheSexuality.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using UnityEngine;
+using Verse;
 
 namespace Maux36.RimPsyche.Sexuality
 {
@@ -9,6 +10,77 @@ namespace Maux36.RimPsyche.Sexuality
             if (!ModsConfig.IsActive("maux36.rimpsyche"))
             {
                 Log.Error("[Rimpsyche Sexuality] Rimpsyche not loaded. The dependency was not met and the game will not run correctly");
+            }
+        }
+        public override string SettingsCategory()
+        {
+            return "RimpsycheSexualitySettingCategory".Translate();
+        }
+        private static Vector2 scrollPosition = new Vector2(0f, 0f);
+        private static float totalContentHeight = 400f;
+        private const float ScrollBarWidthMargin = 18f;
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Rect outerRect = inRect.ContractedBy(10f);
+            bool scrollBarVisible = totalContentHeight > outerRect.height;
+            var scrollViewTotal = new Rect(0f, 0f, outerRect.width - (scrollBarVisible ? ScrollBarWidthMargin : 0), totalContentHeight);
+            Widgets.BeginScrollView(outerRect, ref scrollPosition, scrollViewTotal);
+
+            var listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(new Rect(0f, 0f, scrollViewTotal.width, 9999f));
+
+            listing_Standard.Gap(12f);
+            listing_Standard.Label("RimpsycheKinseyDistributionSettings".Translate());
+            Rect sliderArea = listing_Standard.GetRect(200f);
+            DrawKinseyDistributionSliders(sliderArea);
+            listing_Standard.Gap(12f);
+            listing_Standard.CheckboxLabeled("RimpsycheUsePreferenceSystem".Translate(), ref RimpsycheSettings.usePreferenceSystem, "RimpsycheUsePreferenceSystemTooltip".Translate());
+            listing_Standard.Gap(12f);
+            listing_Standard.CheckboxLabeled("RimpsycheRomanceAttemptGenderDiff".Translate(), ref RimpsycheSettings.romanceAttemptGenderDiff, "RimpsycheRomanceAttemptGenderDiffTooltip".Translate());
+
+            listing_Standard.End();
+            Widgets.EndScrollView();
+        }
+        private void DrawKinseyDistributionSliders(Rect rect)
+        {
+            float total = 0f;
+            foreach (var value in RimpsycheSettings.KinseyDistributionSetting)
+            {
+                total += value;
+            }
+            const int numSliders = 7;
+            float spacing = 5f;
+            float sliderWidth = 50f;
+            float sliderHeight = rect.height - 100f; // Leave space for labels
+            string pcText;
+
+            for (int i = 0; i < numSliders; i++)
+            {
+                float x = rect.x + i * (sliderWidth + spacing);
+                Widgets.Label(new Rect(x, rect.y, sliderWidth, 20f), i.ToString());
+                Rect sliderRect = new Rect(x, rect.y + 20f, sliderWidth, sliderHeight);
+                RimpsycheSettings.KinseyDistributionSetting[i] = (int)(GUI.VerticalSlider(
+                    sliderRect,
+                    RimpsycheSettings.KinseyDistributionSetting[i],
+                    100f,
+                    0f
+                ));
+
+                Rect valLabelRect = new Rect(x, sliderRect.yMax, sliderWidth, 20f);
+                Widgets.Label(valLabelRect, RimpsycheSettings.KinseyDistributionSetting[i].ToString());
+                Rect pcLabelRect = new Rect(x, valLabelRect.yMax, sliderWidth, 20f);
+                if (total == 0f) pcText = "(" + (1f / 7f).ToStringPercent() + ")";
+                else pcText = "(" + (RimpsycheSettings.KinseyDistributionSetting[i] / total).ToStringPercent() + ")";
+                Widgets.Label(pcLabelRect, pcText);
+            }
+            Rect buttonRect = new Rect(rect.x, rect.yMax - 30f, 300f, 30f);
+            if (Widgets.ButtonText(buttonRect, "RimpsycheKinseyDefaultSetting".Translate()))
+            {
+                //Slightly adjusted from https://yougov.co.uk/society/articles/12999-half-young-not-heterosexual
+                for (int i = 0; i < numSliders; i++)
+                {
+                    RimpsycheSettings.KinseyDistributionSetting[i] = RimpsycheSettings.DefaultDistribution[i];
+                }
             }
         }
     }

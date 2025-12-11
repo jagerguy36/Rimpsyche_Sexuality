2fa80929a172088effd631d0b5ba195c42fbb891
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-11 15:34:11 +0900

    Feat: Should earn relationship point after gaining intimacy

---
+++ Source/Mods/Rimpsyche_Intimacy_Compat/Relationship/SexUtilities_ApplyLovinThoughts_Patch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using LoveyDoveySexWithEuterpe;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_Intimacy_Compat
+{
+    [HarmonyPatch(typeof(SexUtilities), nameof(SexUtilities.ApplyLovinThoughts))]
+    public static class SexUtilities_ApplyLovinThoughts_Patch
+    {
+        public static void Postfix(Pawn pawn, Pawn partner)
+        {
+            var pawnPsyche = pawn.compPsyche();
+            var partnerPsyche = partner.compPsyche();
+            if (pawnPsyche?.Enabled == true && partnerPsyche?.Enabled == true)
+            {
+                pawnPsyche.Sexuality.IncrementRelationshipWith(partner, 0.01f);
+                partnerPsyche.Sexuality.IncrementRelationshipWith(pawn, 0.01f);
+            }
+        }
+    }
+}

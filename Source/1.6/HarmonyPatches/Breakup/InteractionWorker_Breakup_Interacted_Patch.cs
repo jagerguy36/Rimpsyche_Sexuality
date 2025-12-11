4e586ba1cd43cf727b6f71e87972a83f8ceefb37
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-12 01:43:51 +0900

    Feat: Locale, relation patch

---
+++ Source/1.6/HarmonyPatches/Breakup/InteractionWorker_Breakup_Interacted_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality
+{
+    [HarmonyPatch(typeof(InteractionWorker_Breakup), "Interacted")]
+    public static class InteractionWorker_Breakup_Interacted_Patch
+    {
+        public static void Postfix(Pawn initiator, Pawn recipient)
+        {
+            var iniPsyche = initiator.compPsyche();
+            var reciPsyche = recipient.compPsyche();
+            if (iniPsyche?.Enabled != true || reciPsyche?.Enabled != true) return;
+            iniPsyche.Sexuality.ClampRelationshipWith(recipient, 0.05f);
+            reciPsyche.Sexuality.ClampRelationshipWith(initiator, 0.05f);
+        }
+    }
+}

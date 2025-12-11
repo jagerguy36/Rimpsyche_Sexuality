53e82efebc3b6d7fb76d1aac967067a5bde3a900
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-11 15:35:49 +0900

    Feat: Delay RJW unpatching

--- Source/Mods/Rimpsyche_RJW_Compat/HarmonyInit.cs
+++ Source/Mods/Rimpsyche_RJW_Compat/HarmonyInit.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System;
 using System.Reflection;
 using Verse;
 
@@ -8,17 +9,23 @@ namespace Maux36.RimPsyche.Sexuality.Rimpsyche_RJW_Compat
     [StaticConstructorOnStartup]
     public static class HarmonyInit
     {
+        private static Harmony harmony;
         static HarmonyInit()
         {
             Log.Message("[Rimpsyche - Sexuality] Patching RJW");
-            MethodInfo RJW_TraitSet_GainTrait_Patch_fixes = typeof(TraitSet).GetMethod("GainTrait");
-            MethodInfo RJW_TraitSet_RemoveTrait_Patch_fixes = typeof(TraitSet).GetMethod("RemoveTrait");
+            harmony = new Harmony("rimworld.mod.Maux.RimPsyche.Sexuality.RJW");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            LongEventHandler.QueueLongEvent(DelayedUnpatch, "RPS_RJW_Unpatching", false, null);
+        }
+        static void DelayedUnpatch()
+        {
+            //Unpatch RJW
+            MethodInfo RJW_TraitSet_GainTrait_Patch_fixes = typeof(TraitSet).GetMethod("GainTrait", new Type[] { typeof(Trait), typeof(bool) });
+            MethodInfo RJW_TraitSet_RemoveTrait_Patch_fixes = typeof(TraitSet).GetMethod("RemoveTrait", new Type[] { typeof(Trait), typeof(bool) });
             MethodInfo RJW_CompAbilityEffect_WordOfLove_ValidateTarget_Patch_fixes = typeof(CompAbilityEffect_WordOfLove).GetMethod("ValidateTarget");
-            var harmony = new Harmony("rimworld.mod.Maux.RimPsyche.Sexuality.RJW");
             harmony.Unpatch(RJW_TraitSet_GainTrait_Patch_fixes, HarmonyPatchType.Postfix, "rjw");
             harmony.Unpatch(RJW_TraitSet_RemoveTrait_Patch_fixes, HarmonyPatchType.Postfix, "rjw");
             harmony.Unpatch(RJW_CompAbilityEffect_WordOfLove_ValidateTarget_Patch_fixes, HarmonyPatchType.Prefix, "rjw");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }

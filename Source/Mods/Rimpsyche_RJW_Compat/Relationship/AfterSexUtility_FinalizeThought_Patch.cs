0918b9f0d68826f9e6067e9ff15b8b72d7b0e886
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-11 15:34:55 +0900

    Feat: RJW lovin should earn relationship as long as it was done with consent

---
+++ Source/Mods/Rimpsyche_RJW_Compat/Relationship/AfterSexUtility_FinalizeThought_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using rjw;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_RJW_Compat
+{
+    [HarmonyPatch(typeof(AfterSexUtility), nameof(AfterSexUtility.FinalizeThought))]
+    public static class AfterSexUtility_FinalizeThought_Patch
+    {
+        public static void Postfix(ThoughtDef thoughtgain, Pawn pawn, Pawn partner)
+        {
+            if (thoughtgain == ThoughtDefOf.GotSomeLovin)
+            {
+                var pawnPsyche = pawn.compPsyche();
+                if (pawnPsyche?.Enabled == true)
+                {
+                    pawnPsyche.Sexuality.IncrementRelationshipWith(partner, 0.01f);
+                }
+            }
+        }
+    }
+}

fd7fd9267af91e168a66624c78ffafb47d32db8d
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-12 21:26:00 +0900

    Feat: Use RJW logic for asexuality

--- Source/Mods/Rimpsyche_RJW_Compat/Orientation/CompRJW_UpdateOrientation_Patch.cs
+++ Source/Mods/Rimpsyche_RJW_Compat/Orientation/CompRJW_UpdateOrientation_Patch.cs
@@ -14,12 +14,12 @@ namespace Maux36.RimPsyche.Sexuality.Rimpsyche_RJW_Compat
             {
                 return true;
             }
-            var observerPsyche = pawn.compPsyche();
-            if (observerPsyche?.Enabled != true)
+            var pawnPsyche = pawn.compPsyche();
+            if (pawnPsyche?.Enabled != true)
             {
                 return true;
             }
-            var o = observerPsyche.Sexuality.orientationCategory;
+            var o = pawnPsyche.Sexuality.orientationCategory;
             if (o == SexualOrientation.Asexual || o == SexualOrientation.Developing)
             {
                 compRJW.orientation = Orientation.Asexual;
@@ -27,10 +27,19 @@ namespace Maux36.RimPsyche.Sexuality.Rimpsyche_RJW_Compat
             }
             else if(o == SexualOrientation.None)
             {
-                compRJW.orientation = Orientation.None;
+                compRJW.orientation = Orientation.Asexual;
                 return false;
             }
-            int k = observerPsyche.Sexuality.GetKinseyReport();
+
+            //Let RJW logic decide coital asexuality, since their separate logic might depend on it.
+            if (!Genital_Helper.has_genitals(pawn) && (pawn.kindDef.race.defName.ToLower().Contains("droid") || pawn.kindDef.race.defName.ToLower().Contains("drone")))
+            {
+                compRJW.orientation = Orientation.Asexual;
+                return false;
+            }
+
+            //IF the pawn is sexually active, then respect Psyche sexuality.
+            int k = pawnPsyche.Sexuality.GetKinseyReport();
             switch(k)
             {
                 case 0:

dc7347eedd789f3949a64cb381fe10a890a6f357
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-13 20:04:17 +0900

    Feat: RJW sexchange support

---
+++ Source/Mods/Rimpsyche_RJW_Compat/SexChange/GenderHelper_ChangeSex_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using rjw;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_RJW_Compat
+{
+    [HarmonyPatch(typeof(GenderHelper), nameof(GenderHelper.ChangeSex), [typeof(Pawn), typeof(GenderHelper.Sex), typeof(GenderHelper.Sex)])]
+    public static class GenderHelper_ChangeSex_Patch
+    {
+        public static void Postfix(Pawn pawn)
+        {
+            var compPsyche = pawn.compPsyche();
+            if (compPsyche?.Enabled != true) return;
+            compPsyche.Sexuality.Notify_Sexchange();
+        }
+    }
+}

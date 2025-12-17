e8ab4b3fa34fdd0fd34dfcec06be4df146106edf
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-18 00:56:32 +0900

    Feat: Intimacy fix mutual attraction condition, since attracted to gender cannot account for asexuality well

---
+++ Source/Mods/Rimpsyche_Intimacy_Compat/Orientation/CommonChecks_AreMutuallyAttracted_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using LoveyDoveySexWithEuterpe;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_Intimacy_Compat
+{
+    [HarmonyPatch(typeof(CommonChecks), nameof(CommonChecks.AreMutuallyAttracted))]
+    public static class CommonChecks_AreMutuallyAttracted_Patch
+    {
+        public static bool Prefix(ref bool __result, Pawn a, Pawn b)
+        {
+            var aPsyche = a.compPsyche();
+            var bPsyche = b.compPsyche();
+            if (aPsyche.Enabled != true || bPsyche.Enabled != true) return true;
+            __result = aPsyche.Sexuality.GetAdjustedAttraction(b) > 0f && bPsyche.Sexuality.GetAdjustedAttraction(a) > 0f;
+            return false;
+        }
+    }
+}
--- Source/Mods/Rimpsyche_Intimacy_Compat/Orientation/CommonChecks_AttractedToGender_Patch.cs
+++ Source/Mods/Rimpsyche_Intimacy_Compat/Orientation/CommonChecks_AttractedToGender_Patch.cs
@@ -7,11 +7,12 @@ namespace Maux36.RimPsyche.Sexuality.Rimpsyche_Intimacy_Compat
     [HarmonyPatch(typeof(CommonChecks), nameof(CommonChecks.AttractedToGender))]
     public static class CommonChecks_AttractedToGender_Patch
     {
-        public static void Postfix(ref bool __result, Pawn pawn, Gender gender)
+        public static bool Prefix(ref bool __result, Pawn pawn, Gender gender)
         {
             var compPsyche = pawn.compPsyche();
-            if (compPsyche.Enabled != true) return;
+            if (compPsyche.Enabled != true) return true;
             __result = compPsyche.Sexuality.CanFeelAttractionToGender(gender);
+            return false;
         }
     }
 }

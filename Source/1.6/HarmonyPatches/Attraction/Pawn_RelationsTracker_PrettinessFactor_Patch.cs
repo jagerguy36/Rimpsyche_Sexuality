1a29695035229ba98c00cd7f1c5b4c13d838f3e2
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-18 00:55:10 +0900

    Feat: Prettyness factor pawn beauty fix

--- Source/1.6/HarmonyPatches/Attraction/Pawn_RelationsTracker_PrettinessFactor_Patch.cs
+++ Source/1.6/HarmonyPatches/Attraction/Pawn_RelationsTracker_PrettinessFactor_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace Maux36.RimPsyche.Sexuality
@@ -7,21 +8,32 @@ namespace Maux36.RimPsyche.Sexuality
     [HarmonyPatch(typeof(Pawn_RelationsTracker), nameof(Pawn_RelationsTracker.PrettinessFactor))]
     public static class Pawn_RelationsTracker_PrettinessFactor_Patch
     {
-        public static void Postfix(ref float __result, Pawn ___pawn, Pawn otherPawn)
+        [HarmonyPriority(Priority.First)]
+        public static bool Prefix(ref float __result, Pawn ___pawn, Pawn otherPawn)
         {
-            var pawnPsyche = ___pawn.compPsyche();
-            if (pawnPsyche?.Enabled != true) return;
-            if (__result == 1f) return;
-            if (__result < 1f)
+            float num = 0f;
+            if (otherPawn.RaceProps.Humanlike)
             {
+                num = otherPawn.GetStatValue(StatDefOf.PawnBeauty);
+            }
+            if (num < 0f)
+            {
+                __result = Mathf.Max(1f / 6f, (1f / 3f) * num + 1f);
+                var pawnPsyche = ___pawn.compPsyche();
+                if (pawnPsyche?.Enabled != true) return false;
                 __result *= pawnPsyche.Evaluate(AuthenticUglinessMultiplier);
-                return;
+                return false;
             }
-            else
+            if (num > 0f)
             {
+                __result = Mathf.Min(3.5f, num + 1f);
+                var pawnPsyche = ___pawn.compPsyche();
+                if (pawnPsyche?.Enabled != true) return false;
                 __result *= pawnPsyche.Evaluate(AuthenticBeautyMultiplier);
-                return;
+                return false;
             }
+            __result = 1f;
+            return false;
         }
         public static RimpsycheFormula AuthenticUglinessMultiplier = new(
             "AuthenticUglinessMultiplier",

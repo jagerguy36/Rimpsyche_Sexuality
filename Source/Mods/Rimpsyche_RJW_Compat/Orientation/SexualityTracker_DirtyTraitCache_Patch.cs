2324841f7cdc8bf3ded15e40bedd1ea220603d19
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-20 18:41:32 +0900

    Feat: Unless being generated, RJW orientaion needs to be checked as soon as RPS sexuality is dirtied aka sexual trait changed. Call RPS to validate its sexuality as soon as sexuality is dirtied, which will call AssignSexuality if needed and update RJW orientation.

---
+++ Source/Mods/Rimpsyche_RJW_Compat/Orientation/SexualityTracker_DirtyTraitCache_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_RJW_Compat
+{
+    [HarmonyPatch(typeof(Pawn_SexualityTracker), nameof(Pawn_SexualityTracker.DirtyTraitCache))]
+    public static class SexualityTracker_DirtyTraitCache_Patch
+    {
+        public static void Postfix(Pawn_SexualityTracker __instance, bool __result)
+        {
+            if (!__result) return;
+            var pawn = __instance.pawn;
+            if (PawnGenerator.IsBeingGenerated(pawn))
+            {
+                return;
+            }
+            __instance.Validate();
+        }
+    }
+}

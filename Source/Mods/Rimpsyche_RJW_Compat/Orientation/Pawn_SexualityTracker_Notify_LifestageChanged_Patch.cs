9f2f392fcbd441a7b3199bdd105a43166371724b
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2026-01-06 22:50:53 +0900

    Feat: Lifestage change notification

---
+++ Source/Mods/Rimpsyche_RJW_Compat/Orientation/Pawn_SexualityTracker_Notify_LifestageChanged_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using rjw;
+using Verse;
+
+namespace Maux36.RimPsyche.Sexuality.Rimpsyche_RJW_Compat
+{
+    [HarmonyPatch(typeof(Pawn_SexualityTracker), "Notify_LifestageChanged")]
+    public static class Pawn_SexualityTracker_Notify_LifestageChanged_Patch
+    {
+        public static void Postfix(Pawn_SexualityTracker __instance)
+        {
+            var pawn = __instance.pawn;
+            if (PawnGenerator.IsBeingGenerated(pawn))
+            {
+                return;
+            }
+            CompRJW.UpdateOrientation(pawn);
+        }
+    }
+}
